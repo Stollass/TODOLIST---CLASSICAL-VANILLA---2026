0e9f7190e4cd18d9d3877f7ed1d97610f5043119
Stollass <natacha.muset@gmail.com>
2026-05-18 13:20:23 +0200

    NotcompletedCount

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
+// Initialisation d'un localStorage avec des todos
+// localStorage.todos = JSON.stringify([
+//   { id: 1, content: "Tâche 1", completed: true },
+//   { id: 2, content: "Tâche 2", completed: false },
+// ]);
 
-    // Initialisation d'un localStorage avec todos
-    // localStorage.todos = JSON.stringify([
-    //     {id: 1, content: "Tâche 1 ", completed: true},
-    //     {id: 2, content: "Tâche 2", completed: false}
-    // ]);
 
-    const todos = JSON.parse(localStorage.todos) || [];
-    const appElement = document.querySelector('.todoapp');
-    const addInputElement = appElement.querySelector('.new-todo');
-    const todosContainer = appElement.querySelector('.todo-list');
-    const notCompletedCountElement = appElement.querySelector('.todo-count');
+const todos = JSON.parse(localStorage.todos) || [];
 
-    todos.forEach(item =>{
-        const newItem = document.createElement('div');
-        todosContainer.append(newItem);
-        newItem.outerHTML =`
-        <li data-id="${item.id}" class="${item.completed ? 'completed' : ''}">
+
+// ÉLÉMENTS UTILES DU DOM
+const appElement = document.querySelector(".todoapp");
+const addInputElement = appElement.querySelector(".new-todo");
+const todosContainer = appElement.querySelector(".todo-list");
+const notCompletedCountElement = appElement.querySelector(".todo-count span");
+
+
+// AFFICHAGE DES TODOS INITIALES
+todos.forEach((item) => {
+  const newItem = document.createElement("div");
+  todosContainer.append(newItem);
+  newItem.outerHTML = `
+        <li data-id="${item.id}" class="${item.completed ? "completed" : ""}">
             <div class="view">
-                <input class="toggle" type="checkbox" ${item.completed ? "checked" : ""} />
+              <input class="toggle" type="checkbox" ${item.completed ? "checked" : ""} />
                 <label>${item.content}</label>
                 <button class="destroy"></button>
             </div>
-            <input class="edit" value="${item.content}"/>
-        </li>
-        `;
-    });
+            <input class="edit" value="${item.content}" />
+          </li>
+    `;
+});
+
+// Modifier le contenu du notCompletedCountElement 
+notCompletedCountElement.innerText = todos.filter(
+    (todo) => !todo.completed,
+).length;
